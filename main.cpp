fad08866cf2e6af7acce262f9ffc90a2d5765307
RedAlexDad <91482353+RedAlexDad@users.noreply.github.com>
2022-11-19 15:39:57 +0300

    Первое задание "функция" добавлено

--- main.cpp
+++ main.cpp
@@ -1,163 +1,259 @@
 /*
-Описать структуру с именем AEROFLOT, содержащую следующие поля:
-название пункта назначения рейса; номер рейса; тип самолета.
+Написать программу, в которой тестируется пользовательская функция.
+ В программе перегрузить данную функцию для указателей на переменные и для ссылок. Варианты функций см. в таблице:
 
-Написать программу, выполняющую следующие действия:
-ввод с клавиатуры данных в массив, состоящий из семи элементов типа AEROFLOT;
-записи должны быть упорядочены по возрастанию номера рейса;
-вывод на экран номеров рейсов и типов самолетов, вылетающих в пункт назначения,
-название которого совпало с названием, введенным с клавиатуры;
-если таких рейсов нет, выдать на дисплей соответствующее сообщение.
+1.	Сформировать динамический одномерный массив, заполнить его случайными числами и вывести на печать.
+2.	Выполнить указанное в варианте задание и вывести полученный массив на печать.
+3.	Сформировать динамический двумерный массив, заполнить его случайными числами и вывести на печать.
+4.	Выполнить указанное в варианте задание и вывести полученный массив на печать.
+
+№	Процедура
+4	Удалить элемент равный среднему арифметическому элементов массива.
+
 */
 
 
 #include <iostream>
-#include <cstring>
+#include <cmath>
 
 using namespace std;
 
-// Структура Аэропорта
-struct AEROFLOT
+double discarding_value(double& value)
 {
-    // Имя рейса
-    char *name_reis;
+    // Проверка на округление, если округляется в большую сторону, т.е 1,5 = 2
+    // То положительный, иначе отрицательный
+    double check = value - round(value);
 
-    // Номер рейса
-    int num_reis;
+    if (check > 0)
+        return value = value - round(value);
+    else
+        return value = value - int(value);
+}
 
-    // Тип самолета
-    char *type_plane;
-};
-
-// Пользовательский ввод с клавиатуры данными в массив структуры
-void cin_airflot(AEROFLOT* air)
+double discarding_value(double* value)
 {
-    char* buf = new char[1000];
-    cout << "Введите имя рейса: ";
-    cin >> buf;
+    // Проверка на округление, если округляется в большую сторону, т.е 1,5 = 2
+    // То положительный, иначе отрицательный
+    double check = *value - round(*value);
 
-    // Выделяем память на заполнение данных
-    air->name_reis = new char[strlen(buf)];
+    if (check > 0)
+        return *value = *value - round(*value);
+    else
+        return *value = *value - int(*value);
+}
 
-    strcpy_s(air->name_reis, strlen(buf) + 1, buf);
-    //strcpy(air.name_reis, buf);
+void func_create_array(int* mas, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        mas[i] = rand() % 50;
+        cout << mas[i] << ' ';
+    }
+    cout << endl << endl;
+}
 
-    // Освобождение памяти и выделение новой памяти
-    delete[] buf;
-    buf = new char[1000];
+void func_create_array(int** matr, int row, int col)
+{
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            matr[i][j] = rand() % 50;
+            cout << matr[i][j] << ' ';
+        }
+        cout << endl;
+    }
+    cout << endl << endl;
+}
 
-    cout << "Введите номер рейса: ";
-    cin >> air->num_reis;
+void find_arifm_delete(int* mas, int size)
+{
+    int sum = 0;
 
-    cout << "Введите тип самолета: ";
-    cin >> buf;
+    // Находит среднее арифметическое число
+    for (int i = 0; i < size; i++)
+        sum += mas[i];
 
-    // Выделяем память на заполнение данных
-    air->type_plane = new char[strlen(buf)];
+    double arifm_mas = sum / size;
+    cout << "Среднее арифметическое число: " << arifm_mas << endl;
 
-    strcpy_s(air->type_plane, strlen(buf) + 1, buf);
+    // Временный массив
+    int* buf = new int[size];
+    int k = 0;
+
+    // Сохраняет все значения отличаемое от среднего арифметического значения
+    for (int i = 0; i < size; i++) {
+        if (arifm_mas != mas[i]) {
+            buf[k] = mas[i];
+            k++;
+        }
+    }
+
+    // Передаем размер массива в size
+    size = k;
+
+    // Освобождаем, и опять выделяем память с другим размером
+    delete[] mas;
+    mas = new int[size];
+
+    for (int i = 0; i < k; i++)
+        mas[i] = buf[i];
 
     // Освобождаем памяти
     delete[] buf;
+
+    for (int i = 0; i < size; i++)
+        cout << mas[i] << ' ';
+
+    cout << endl << endl;
+
+    delete[] mas;
 }
 
-// Печать структуры
-void print_airflot(AEROFLOT air)
+void find_arifm_delete(int** matr, int row, int col)
 {
-    cout << "Имя рейса: " << air.name_reis << endl;
-    cout << "Номер рейса: " << air.num_reis << endl;
-    cout << "Тип самолета: " << air.type_plane << endl;
-}
+    int sum = 0, k = 0;
 
-// Проверка на одинаковые названия рейса, введенного пользователем
-void check_matches(AEROFLOT* AIRBUS, int size_airflot, int *check_warn)
-{
-    char* buf = new char[1000];
+    // Находит среднее арифметическое число
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < col; j++)
+            sum += matr[i][j];
 
-    cout << "Введите название рейс, которого хотите искать" << endl;
-    cin >> buf;
+    double arifm_mas = sum / (row * col);
+    cout << "Среднее арифметическое число: " << arifm_mas << endl;
 
-    char* name = new char[strlen(buf) + 1];
+    // Временный массив
+    int* buf = new int[row*col];
 
-    strcpy(name, buf);
-
-    cout << "Имя рейса: " << name << endl << endl;
-
-    delete[] buf;
-
-    for (int i = 0; i < size_airflot; i++)
-        if (strcmp(name, AIRBUS[i].name_reis) == 0)
-        {
-            cout << "Совпадений найдено: " << endl;
-            //cout << "Имя рейса: " << AIRBUS[i].name_reis << " и " << AIRBUS[j].name_reis << endl;
-            cout << "Имя рейса: " << AIRBUS[i].name_reis << endl;
-            cout << "Номер рейса: " << AIRBUS[i].num_reis << endl;
-            cout << "Тип самолета: " << AIRBUS[i].type_plane << endl;
-            cout << endl << endl;
-
-            // Найдено совпадний, тогда нет ошибок
-            *check_warn = 0;
-        }
-
-    delete[] name;
-}
-
-// Сортировка массива по возрастанию
-void sort_airflot(AEROFLOT* air, int size_airflot)
-{
-    for (int i = 0; i < size_airflot; i++) {
-        for (int j = 0; j < size_airflot; j++) {
-            if (air[i].num_reis < air[j].num_reis) {
-                AEROFLOT tmp = air[i];
-                air[i] = air[j];
-                air[j] = tmp;
+    // Сохраняет все значения отличаемое от среднего арифметического значения
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            if (arifm_mas != matr[i][j]) {
+                buf[k] = matr[i][j];
+                k++;
             }
         }
     }
+
+    // Передаем размер массива в size
+    int mas_size = k;
+
+    int* mas = new int[mas_size];
+
+    for (int i = 0; i < mas_size; i++) {
+        // Перезапись поэлементно
+        mas[i] = buf[i];
+        cout << mas[i] << ' ';
+    }
+
+    // Освобождаем памяти
+    delete[] buf;
+    delete[] mas;
+    delete[] matr;
+
+    cout << endl << endl;
 }
 
 
 int main() {
     // system("chcp 1251 > nul");
     system("chcp 65001");
+    srand(time(NULL));
 
     cout << "Лабораторная работа № 1\n Папин А.В., ИУ5Ц-41Б \n Вариант № 4\n\n";
+    cout << "Выберите номер пункта для выполнения задач: \n";
+    cout << "1. Индивидуальное задание №1\n";
+    cout << "2. Индивидуальное задание №2\n";
+    cout << "0. Выход с программы\n";
+    cout << "Работа программы:\n\n";
 
-    const int size_airflot = 3;
-    // Создаем и выделяем память
-    AEROFLOT *AIRBUS = new AEROFLOT[size_airflot];
+    int sw;
+    cin >> sw;
 
-    cout << "Введите все данные для аэропорта" << endl;
+    switch (sw)
+    {
+        case 1:
+        {
+            // Индивидуальное задание №1.
+            /* Написать программу, в которой тестируется пользовательская функция.
+            В программе перегрузить данную функцию для указателей на переменные и для ссылок.
+            Варианты функций см. в таблице:*/
 
-    for (int i = 0; i < size_airflot; i++) {
-        cout << "AIRBUS №" << i + 1 << endl;
-        // Пользовательский ввод с клавиатуры данных
-        cin_airflot(&AIRBUS[i]);
+            /*Отбрасывает от вещественного числа его целую часть.*/
+            cout << "1. Индивидуальное задание №1\n";
+
+            double value_double;
+
+            cout << "Примечание: Знак для десятичных дробей следует поставить точкой\n";
+            cout << "Введите любое вещественное (double) число: ";
+            cin >> value_double;
+            cout << discarding_value(value_double) << endl;
+
+            cin.ignore();
+
+            cout << "Введите любое вещественное (double) число: ";
+            cin >> value_double;
+            cout << discarding_value(&value_double) << endl;
+
+            system("pause");
+            break;
+        }
+        case 2:
+        {
+            // Индивидуальное задание №2.
+            /* Написать программу, в которой тестируется пользовательская функция.
+            В программе перегрузить данную функцию для указателей на переменные и для ссылок.
+            Варианты функций см. в таблице:
+            Сформировать динамический одномерный массив, заполнить его случайными числами и вывести на печать
+            Выполнить указаннеое в варианте задание и вывести полученный массив на печать
+            Сформировать динамический двумерный массив, заполнить его случайными числами и вывести на печать
+            Выполнить указанное в варианте задание и вывести полученный массив на печать*/
+
+            /* Удалить элемент равный среднему арифметическому элементов массива*/
+            cout << "2. Индивидуальное задание №2\n";
+
+            // Одномерный массив
+
+            int size = 10;
+            int* mas = new int[size];
+
+            // Для одномерного массива
+            func_create_array(mas, size);
+
+            // Находит среднее арифметическое число
+            find_arifm_delete(mas, size);
+
+            // Двумерный массив
+
+            const int row = 3;
+            const int col = 3;
+
+            int** matr = new int*[row];
+            for (int i = 0; i < row; i++)
+                matr[i] = new int[col];
+
+            // Для двумерного массива (матрица)
+            func_create_array(matr, row, col);
+
+            // Находит среднее арифметическое число
+            find_arifm_delete(matr, row, col);
+
+            system("pause");
+            break;
+        }
+        default:
+        {
+            cout << "Не существует такого пункта. Выход с программы\n";
+            system("exit");
+            system("pause");
+            break;
+        }
+        case 0:
+        {
+            cout << "Выход с программы\n";
+            system("exit");
+            system("pause");
+            break;
+        }
     }
-
-    // Сортировка номера рейса по возрастанию
-    sort_airflot(AIRBUS, size_airflot);
-
-
-    cout << endl << endl;
-
-    for (int i = 0; i < size_airflot; i++) {
-        cout << "AIRBUS №" << i + 1 << endl;
-        // Печать на консольное окно
-        print_airflot(AIRBUS[i]);
-    }
-
-    cout << endl << endl;
-
-    // Проверка, если есть совпадений названий, то 0, иначе 1
-    int check_warn = 1;
-
-    check_matches(AIRBUS, size_airflot, &check_warn);
-
-    // Проверка на совпадении названий, введенный пользователем
-    if (check_warn == 1)
-        cout << "Совпадений не найдено\n" << endl;
-
-    // Освобождаем память
-    delete[] AIRBUS;
 }
